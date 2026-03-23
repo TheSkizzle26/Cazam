0bc845189f97d0931ee07bf2d35c92661ae5c09c
TheSkizzle26 <timobarth12@outlook.de>
2026-03-23 20:05:36 +0100

    added build files for linux

---
+++ build/build_fish.sh
@@ -0,0 +1,10 @@
+source ../.venv/bin/activate.fish
+
+# generate executable
+python -m PyInstaller --onefile --windowed --name cazam ../main.py
+cp -r ../shaders dist/
+
+# generate zip file
+cp -r dist cazam
+zip -r cazam.zip cazam/cazam cazam/shaders
+rm -rf cazam
