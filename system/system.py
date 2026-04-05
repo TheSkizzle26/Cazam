f7f4b152ffe4f24d744217f85ce6632c66618c28
TheSkizzle26 <timobarth12@outlook.de>
2026-04-05 10:54:44 +0200

    added system documentation

--- system/system.py
+++ system/system.py
@@ -1,24 +1,48 @@
 class System:
     def fetch(self):
+        """
+        Fetches all the system information.
+        """
         ...
 
     def is_song_playing(self) -> bool:
+        """
+        :return: is a song currently playing?
+        """
         ...
 
     def get_song_path(self) -> str:
+        """
+        :return: current song's local file path, empty if not provided
+        """
         ...
 
     def get_song_name(self) -> str:
+        """
+        :return: the current song's name/title
+        """
         ...
 
     def get_song_album(self) -> str:
+        """
+        :return: the current song's album name
+        """
         ...
 
     def get_song_artists(self) -> list[str]:
+        """
+        :return: a list containing the names of the song's artists
+        """
         ...
 
     def get_song_length(self) -> float:
+        """
+        :return: the current song's total length in seconds
+        """
         ...
 
     def get_song_pos(self) -> float:
+        """
+        :return: get the current position the player is at inside the song, in seconds
+        """
         ...
