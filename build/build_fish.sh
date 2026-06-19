e4f1fa2a99f9e7a7b63408a6d69f57675251311d
TheSkizzle26 <timobarth12@outlook.de>
2026-06-19 22:25:00 +0200

    removed /

--- build/build_fish.sh
+++ build/build_fish.sh
@@ -2,7 +2,7 @@ source ../.venv/bin/activate.fish
 
 # generate executable
 python -m PyInstaller --onefile --windowed --name cazam ../src/main.py
-cp -r ../src/shaders dist/
+cp -r ../src/shaders dist
 
 # generate zip file
 cp -r dist cazam
